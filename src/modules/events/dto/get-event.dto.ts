fe0f47a7477bd0eb15eedfe8b47e592203f2a245
nguyenduc37 <nguyenduc372001@gmail.com>
2022-05-12 13:47:25 +0700

    feat: sort event

--- src/modules/events/dto/get-event.dto.ts
+++ src/modules/events/dto/get-event.dto.ts
@@ -1,3 +1,13 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
 import { PaginationInput } from 'src/shares/pagination/pagination.dto';
+import { ESortEvent } from '../enums/event-type.enum';
 
-export class GetAllEventDto extends PaginationInput {}
+export class GetAllEventDto extends PaginationInput {
+  @ApiPropertyOptional({
+    enum: ESortEvent,
+  })
+  @IsEnum(ESortEvent)
+  @IsOptional()
+  orderBy?: ESortEvent;
+}
