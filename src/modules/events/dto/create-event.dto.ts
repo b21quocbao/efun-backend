9a1a2c90c8a01c088a068b411718e4cbee813bdf
Bao Ngo <ngobao2001@gmail.com>
2022-05-18 17:28:37 +0700

    feat: class validator swagger create event api

--- src/modules/events/dto/create-event.dto.ts
+++ src/modules/events/dto/create-event.dto.ts
@@ -1,19 +1,65 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 import { EventType } from '../enums/event-type.enum';
 
 export class CreateEventDto {
+  @ApiProperty()
+  @IsNumber()
   userId: number;
+
+  @ApiProperty()
+  @IsString()
   name: string;
+
+  @ApiProperty()
+  @IsString()
   thumbnailUrl: string;
+
+  @ApiProperty()
+  @IsNumber()
   categoryId: number;
+
+  @ApiProperty()
+  @IsString()
   type: EventType;
+
+  @ApiPropertyOptional()
+  @IsString()
   scoreData?: string;
+
+  @ApiProperty()
+  @IsDate()
   startTime: Date;
+
+  @ApiProperty()
+  @IsDate()
   deadline: Date;
+
+  @ApiProperty()
+  @IsDate()
   endTime: Date;
+
+  @ApiProperty()
+  @IsString()
   options: string;
+
+  @ApiProperty()
+  @IsString()
   odds: string;
+
+  @ApiPropertyOptional()
+  @IsString()
   streamUrl?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
   result?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
   resultProofUrl?: string;
+
+  @ApiPropertyOptional()
+  @IsNumber()
   transactionId?: number;
 }
