772afe70aa31e05f3103fcf8d9b10dbf4a9bf458
Bao Ngo <ngobao2001@gmail.com>
2022-05-25 10:18:58 +0700

    fix: transform number search category

--- src/modules/categories/dto/search-category.dto.ts
+++ src/modules/categories/dto/search-category.dto.ts
@@ -1,9 +1,11 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class SearchCategoryDto {
   @ApiPropertyOptional()
   @IsNumber()
+  @Transform(({ value }) => Number(value))
   @IsOptional()
   fatherId?: number;
 
