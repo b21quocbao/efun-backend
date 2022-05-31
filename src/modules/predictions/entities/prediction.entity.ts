f2a1e1ec7165acac13b6b7fead7b84b98ec5982c
Bao Ngo <ngobao2001@gmail.com>
2022-05-31 15:27:11 +0700

    fix: unique fields in prediction

--- src/modules/predictions/entities/prediction.entity.ts
+++ src/modules/predictions/entities/prediction.entity.ts
@@ -14,7 +14,7 @@ import {
 } from 'typeorm';
 
 @Entity('predictions')
-@Unique(['userId', 'predictNum'])
+@Unique(['userId', 'predictNum', 'token', 'eventId'])
 export class PredictionEntity {
   @PrimaryGeneratedColumn()
   id: number;
