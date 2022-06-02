de419fad30c16e1208eb7d725384e1a3585a9768
Bao Ngo <ngobao2001@gmail.com>
2022-06-02 14:25:30 +0700

    feat: filter latest event and prediction

---
+++ src/modules/predictions/dto/search-prediction.dto.ts
@@ -0,0 +1,12 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { PSortEvent } from '../enums/prediction-type.enum';
+
+export class SearchPredictionDto {
+  @ApiPropertyOptional({
+    enum: PSortEvent,
+  })
+  @IsEnum(PSortEvent)
+  @IsOptional()
+  orderBy?: PSortEvent;
+}
