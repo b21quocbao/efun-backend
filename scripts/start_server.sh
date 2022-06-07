212365b34b4d33c92105df43da845e0da70893a7
Bao Ngo <ngobao2001@gmail.com>
2022-06-07 12:10:24 +0700

    feat: update script for crawler

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,3 +2,5 @@
 cd /home/ubuntu/efun-be-v2
 pm2 del main 2> /dev/null
 APP_ENV=production pm2 start dist/main.js
+pm2 del crawler 2> /dev/null
+APP_ENV=production pm2 start "node dist/console.js crawl-all 999999999999" --name crawler
