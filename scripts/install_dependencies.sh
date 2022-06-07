f846653e9c69542ca0f1c07a2cc23fede4d27cd0
Bao Ngo <ngobao2001@gmail.com>
2022-06-07 10:33:07 +0700

    ci: install package script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl --silent --location https://rpm.nodesource.com/setup_16.x | bash -
-apt -y install nodejs
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+apt-get -y install nodejs
 npm install -g yarn pm2
 cd /home/ec2-user/efun-be-v2
 yarn install
