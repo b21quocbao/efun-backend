475d421a386e36fb67c7ed91a773f121369fdd3c
Bao Ngo <ngobao2001@gmail.com>
2022-08-02 08:50:47 +0700

    fix: ci cd mainnet

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu/efun-be-v2
 pm2 del main 2> /dev/null
 APP_ENV=staging pm2 start dist/src/main.js
 pm2 del mainnet 2> /dev/null
-APP_ENV=production pm2 start dist/src/main.js
+APP_ENV=production pm2 start dist/src/main.js --name mainnet
 pm2 save
