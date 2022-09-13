b78390ac5ed15d9da01838f08225d5d29f009fc3
Bao Ngo <ngobao2001@gmail.com>
2022-09-13 12:20:14 +0700

    feat: operator contract update result check

--- src/modules/contracts/contracts.console.ts
+++ src/modules/contracts/contracts.console.ts
@@ -129,13 +129,26 @@ export class ContractConsole implements OnModuleInit {
         event.returnValues.eventId,
       );
 
+      if (event.returnValues.caller.toLowerCase() === process.env.OPERATOR) {
+        event.returnValues.index -= 1;
+      }
+
       if (eventEntity) {
-        await this.eventsService.update(eventEntity.id, {
-          result: JSON.parse(eventEntity.options)[event.returnValues.index],
-          finalTime: new Date(event.returnValues.finalTime * 1000),
-          claimTime: new Date(event.returnValues.claimTime * 1000),
-          status: EventStatus.FINISH,
-        });
+        if (event.returnValues.index == -1) {
+          await this.eventsService.update(eventEntity.id, {
+            result: JSON.parse(eventEntity.options)[event.returnValues.index],
+            finalTime: new Date(event.returnValues.finalTime * 1000),
+            claimTime: new Date(event.returnValues.claimTime * 1000),
+            isBlock: true,
+          });
+        } else {
+          await this.eventsService.update(eventEntity.id, {
+            result: JSON.parse(eventEntity.options)[event.returnValues.index],
+            finalTime: new Date(event.returnValues.finalTime * 1000),
+            claimTime: new Date(event.returnValues.claimTime * 1000),
+            status: EventStatus.FINISH,
+          });
+        }
       }
     };
 
